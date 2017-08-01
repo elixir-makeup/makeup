cc487fd76cf43faec882fc4ddb195e49aaf9c431
tmbb <tmbb@campus.ul.pt>
2017-08-01 14:58:58 +0100

    bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sandbox.Mixfile do
   def project do
     [
       app: :makeup,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0",
       start_permanent: Mix.env == :prod,
       deps: deps(),
