e1c5d27e3f14c04904d74b94b6298bbc28a406c3
tmbb <tmbb@campus.ul.pt>
2017-10-13 00:04:10 +0100

    bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Makeup.Mixfile do
   def project do
     [
       app: :makeup,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.0",
       start_permanent: Mix.env == :prod,
       deps: deps(),
