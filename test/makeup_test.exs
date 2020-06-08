c281c8e08b0c38c258f681eb6831d7b6d86af698
José Valim <jose.valim@dashbit.co>
2020-06-08 15:29:28 +0200

    Add tests for stylesheet

--- test/makeup_test.exs
+++ test/makeup_test.exs
@@ -6,16 +6,16 @@ defmodule MakeupTest do
     @behaviour Makeup.Lexer
 
     @impl true
-    def root_element(_), do: raise "Not implemented"
+    def root_element(_), do: raise("Not implemented")
 
     @impl true
-    def root(_), do: raise "Not implemented"
+    def root(_), do: raise("Not implemented")
 
     @impl true
-    def postprocess(_, _), do: raise "Not implemented"
+    def postprocess(_, _), do: raise("Not implemented")
 
     @impl true
-    def match_groups(_, _), do: raise "Not implemented"
+    def match_groups(_, _), do: raise("Not implemented")
 
     @impl true
     def lex("CompiledWithDocs.example", _lexer_options) do
@@ -30,7 +30,7 @@ defmodule MakeupTest do
   describe "highlight_inner_html" do
     test "hightlight using default tag <span>" do
       assert Makeup.highlight_inner_html("CompiledWithDocs.example", lexer: FakeLexer) ==
-        ~S(<span class="nc">CompiledWithDocs</span><span class="o">.</span><span class="n">example</span>)
+               ~S(<span class="nc">CompiledWithDocs</span><span class="o">.</span><span class="n">example</span>)
     end
 
     test "hightlight using custom tag <samp>" do
@@ -38,8 +38,18 @@ defmodule MakeupTest do
       options = [lexer: FakeLexer, formatter_options: formatter_options]
 
       assert Makeup.highlight_inner_html("CompiledWithDocs.example", options) ==
-        ~S(<samp class="nc">CompiledWithDocs</samp><samp class="o">.</samp><samp class="n">example</samp>)
+               ~S(<samp class="nc">CompiledWithDocs</samp><samp class="o">.</samp><samp class="n">example</samp>)
     end
   end
 
+  describe "stylesheet" do
+    test "returns stylesheet in given style" do
+      assert Makeup.stylesheet() =~ ".highlight .hll"
+      assert Makeup.stylesheet(:vim_style) =~ ".highlight .hll"
+      assert Makeup.stylesheet(:vim_style, "makeup") =~ ".makeup .hll"
+
+      assert Makeup.stylesheet(Makeup.Styles.HTML.StyleMap.vim_style(), "makeup") =~
+               ".makeup .hll"
+    end
+  end
 end
