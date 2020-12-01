419c7cef5d563b4a39a6b7466059570bf97d893d
kyleVsteger <kylesteger@gmail.com>
2020-12-01 15:36:49 -0500

    Map.t() -> map() (#33)

--- lib/makeup/lexer/types.ex
+++ lib/makeup/lexer/types.ex
@@ -1,10 +1,9 @@
 defmodule Makeup.Lexer.Types do
-  @type token :: {atom(), Map.t(), iodata()}
+  @type token :: {atom(), map(), iodata()}
   @type tokens :: [token()]
-  @type context :: Map.t()
+  @type context :: map()
   @type parsec_success :: {:ok, tokens, String.t(), context(), integer(), integer()}
   @type parsec_failure :: {:error, String.t(), Sring.t(), context(), {integer(), integer()}, integer()}
   @type parsec_result :: parsec_success | parsec_failure
   @type parsec :: (String.t -> parsec_result)
 end
-
