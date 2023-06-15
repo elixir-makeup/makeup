738a4a6a03e42d38747bc81bff76fac600a1745e
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-06-15 23:24:33 +0100

    Fix type definition
    As can be seen by Dialyzer'ing makeup_erlang or makeup_elixir

--- lib/makeup/lexer/types.ex
+++ lib/makeup/lexer/types.ex
@@ -2,7 +2,7 @@ defmodule Makeup.Lexer.Types do
   @type token :: {atom(), map(), iodata()}
   @type tokens :: [token()]
   @type context :: map()
-  @type parsec_success :: {:ok, tokens, String.t(), context(), integer(), integer()}
+  @type parsec_success :: {:ok, tokens, String.t(), context(), {integer(), integer()}, integer()}
   @type parsec_failure :: {:error, String.t(), String.t(), context(), {integer(), integer()}, integer()}
   @type parsec_result :: parsec_success | parsec_failure
   @type parsec :: (String.t -> parsec_result)
